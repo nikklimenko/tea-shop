9e0dde1dd0e9559616146e6203f1fbadf0baf185
Nik Klimenko <nikklimenko89@gmail.com>
2023-05-17 19:37:21 +0300

    rework accordeon in main on ng-bootstrap

--- src/app/views/main/main.module.ts
+++ src/app/views/main/main.module.ts
@@ -5,6 +5,7 @@ import { MainRoutingModule } from './main-routing.module';
 import {MainComponent} from "./main.component";
 import {SharedModule} from "../../shared/shared.module";
 import {RouterModule} from "@angular/router";
+import {NgbAccordionModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import {RouterModule} from "@angular/router";
     CommonModule,
     SharedModule,
     RouterModule,
+    NgbAccordionModule,
     MainRoutingModule
   ],
   exports: [
